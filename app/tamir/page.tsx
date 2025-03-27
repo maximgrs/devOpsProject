830bcfebf8122cb357e86b41f2d9b9d2c7fec921
Tamir <tamir@htld>
2025-03-27 11:48:50 +0100

    feature: button added

--- app/tamir/page.tsx
+++ app/tamir/page.tsx
@@ -2,6 +2,14 @@ import React from "react";
 
 type Props = {};
 
-export default function page({}: Props) {
-  return <div>code hier yamanyak</div>;
+export default function page({ }: Props) {
+  return (
+    <div>
+      <h1 className="text-lg">Tamir's page</h1>
+      <div className="w-52 p-2 m-5 border-1 border-white hover:bg-gray-800">
+        <p>This is a React component created by Tamir, which does nothing</p>
+      </div>
+      <a href="/" className="border-2 border-white p-2 hover:bg-gray-900">back to homepage</a>
+    </div>
+  );
 }
